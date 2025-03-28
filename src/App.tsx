1b7052c8537df0975d198729935bedf91b24f029
kelter-js <spawnkillerqw@gmail.com>
2025-03-28 16:02:50 +0500

    fead: added new logic render, now app contains search logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,29 @@ import { SearchFeed } from "./components/SearchFeed";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header";
 import { SpecialTags } from "./SpecialTags";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { SPECIALS } from "./entities/Specials";
 import { usePerksStore } from "./hooks/usePerksStore";
+import { Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
 
 const App = () => {
   const [selectedTag, setSelectedTag] = useState<SPECIALS>(SPECIALS.STRENGTH);
-  const data = usePerksStore(selectedTag);
-  console.log(data);
+  const [search, setSearch] = useState("");
+
+  const selectedPerks = usePerksStore(selectedTag);
+  console.log(selectedPerks);
+
+  const renderCards = useMemo(() => {
+    if (search && selectedPerks) {
+      return selectedPerks.filter((perks) =>
+        perks.title.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    return selectedPerks;
+  }, [search, selectedPerks]);
+
+  console.log("renderCards", renderCards);
 
   return (
     <div className="App">
@@ -18,14 +33,36 @@ const App = () => {
 
       <Container>
         <Calculator />
+
         <SpecialTags
           selectedTag={selectedTag}
           setSelectedTag={setSelectedTag}
         />
-        {data?.map((item) => (
-          <img src={item.iconSource} />
-        ))}
-        <SearchFeed />
+
+        <SearchFeed search={search} setSearch={setSearch} />
+
+        <Stack direction="row" gap={2} flexWrap="wrap">
+          {renderCards?.map((item, index) => (
+            <Card
+              sx={{ maxWidth: "15.69%", background: "var(--text-color)" }}
+              key={index}
+            >
+              <CardMedia
+                sx={{ width: "100%", height: 250, objectFit: "contain" }}
+                image={item.iconSource}
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {item.getDescriptionBasedOnStars(item.selectedStars)}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Stack>
       </Container>
     </div>
   );
