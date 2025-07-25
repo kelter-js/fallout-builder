a350e1e98fcbcafaf020e858eeab4bbbd3c3a7b6
kelter-js <spawnkillerqw@gmail.com>
2025-07-25 20:54:24 +0500

    del: removed unnecessary files

--- src/SpecialTags/index.ts
+++
@@ -1,1 +0,0 @@
-export { SpecialTags } from "./SpecialTags";
--- src/components/PickedCard/PickedCard.styled.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from "@mui/material";
-
-export const Card = styled("div")`
-  display: flex;
-  flex-direction: column;
-  width: 160px;
-`;
